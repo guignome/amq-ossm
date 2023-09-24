a99944ede2d41b7381068ad48e627e35b181f0e1
Guillaume Radde <guillaume.radde@redhat.com>
2023-09-24 15:58:49 -0400

    Cleanup.

--- README.md
+++ README.md
@@ -14,15 +14,3 @@ Make sure you have version 2.3
 ```
 oc apply -k openshift/base
 ```
-
-## Inject the sidecars
-
-```
-oc label namespace <mynamespace> istio-injection=enabled
-```
-
-## Patch the amq statfulset
-```
-oc patch statefulset/artemis-ss -p '{"spec":{"template":{"metadata":{"annotations":{"sidecar.istio.io/inject":"true", "traffic.sidecar.istio.io/excludeInboundPorts":"7800,7900"}}}}}'
-# oc patch statefulset/artemis-ss -p '{"spec":{"template":{"metadata":{"annotations":{"sidecar.istio.io/inject":"true"}}}}}'
-```
--- src/main/java/com/redhat/demo/amqossm/Routes.java
+++ src/main/java/com/redhat/demo/amqossm/Routes.java
@@ -1,13 +1,8 @@
 package com.redhat.demo.amqossm;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
-import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.logging.Logger;
@@ -24,47 +19,21 @@ public class Routes extends RouteBuilder {
     Logger log;
 
     private static String MSG_URI = "jms://topic:MSG-TOPIC";
-    private static String THX_URI = "jms://topic:THX-TOPIC";
     private static String CONSUMER_ROUTE_ID = "Consumer";
     private static String PRODUCER_ROUTE_ID = "Producer";
-
-    @Inject
-    private CamelContext ctx;
-
-    private static final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
 
     @Override
     public void configure() throws Exception {
 
-        scheduler.schedule(() -> {
-            try {
-                log.info("Manually starting routes.");
-                ctx.getRouteController().startRoute(CONSUMER_ROUTE_ID);
-                ctx.getRouteController().startRoute(PRODUCER_ROUTE_ID);
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }, startDelay, TimeUnit.MILLISECONDS);
-
         if(consumer) {
             from(MSG_URI)
-            .autoStartup(false)
+            .autoStartup(true)
             .routeId(CONSUMER_ROUTE_ID)
-            .log("Message received : ${body}")
-            ;
-            from("timer://mytimer?period=200")
-                    .autoStartup(false)
-                    .routeId(CONSUMER_ROUTE_ID+2)
-                    .setBody().constant("I'm consumer " + System.getenv("HOSTNAME"))
-                    .to(MSG_URI);
+            .log("Message received : ${body}");
+
         } else {
-            from(THX_URI)
-                    .autoStartup(false)
-                    .routeId(CONSUMER_ROUTE_ID)
-                    .log("You are Welcome : ${body}");
             from("timer://mytimer?period=3000")
-                    .autoStartup(false)
+                    .autoStartup(true)
                     .routeId(PRODUCER_ROUTE_ID)
                     .setBody().constant("Hello Service Mesh from " + System.getenv("HOSTNAME"))
                     .to(MSG_URI);
